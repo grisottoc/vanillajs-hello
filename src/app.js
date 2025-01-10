44b62df8638fb900d190db7d9da83d84223c22e1
grisottoc <sales.mercadodirecto@gmail.com>
2025-01-10 19:31:31 +0000

    claudioUpdate

--- src/app.js
+++ src/app.js
@@ -6,6 +6,52 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  const suits = ["♦", "♥", "♠", "♣"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  
+  const generateCard = () => {
+    
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+    
+    document.getElementById("top-suit").textContent = randomSuit;
+    document.getElementById("bottom-suit").textContent = randomSuit;
+    document.getElementById("number").textContent = randomNumber;
+
+    
+    const isRed = randomSuit === "♦" || randomSuit === "♥";
+    const color = isRed ? "red" : "black";
+
+    document.getElementById("top-suit").style.color = color;
+    document.getElementById("bottom-suit").style.color = color;
+    document.getElementById("number").style.color = color;
+  };
+
+  
+  document
+    .getElementById("generate-btn")
+    .addEventListener("click", generateCard);
+
+  
+  generateCard();
+
+  
+  setInterval(generateCard, 10000);
 };
